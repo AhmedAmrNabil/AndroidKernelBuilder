1f76b1edec81b669c255da58b6bf56dd8f48affd
AhmedAmrNabil <ahmedamr24680@gmail.com>
2025-04-04 15:36:16 +0200

    feat: giving proper credits

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Ascendia Build Script - a52sxq
 # Coded by BlackMesa123 @2023
 # Modified and adapted by RisenID @2024
+# Modified by saadelasfur @2025
 # Modified by btngana24680 @2025
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
